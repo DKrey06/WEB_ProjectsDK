183138fa0eb191ec91c49bb3bdabe4f7702732b6
DKrey06 <mrdmitry2006@mail.ru>
2025-10-04 21:11:55 +1000

    Before the template

--- 2.News_Blog_DK/app.py
+++ 2.News_Blog_DK/app.py
@@ -1,18 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app= Flask(__name__)
 
 @app.route("/")
 def index():
-	return "<h1>Добро пожаловать в Новостной Блог!</h1>"
+	return render_template('index.html')
 
 @app.route("/about")
 def about():
-	return "<h1>about us</h1>"
+	return render_template('about.html')
 
 @app.route("/contact")
 def contact():
-	return "<h1>contact</h1>"
+	return render_template('contact.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
