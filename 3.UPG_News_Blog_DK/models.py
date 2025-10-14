f1402003d81cc30a43f5d6ee29e09f0b1edf1d5e
DKrey06 <mrdmitry2006@mail.ru>
2025-10-14 20:14:35 +1000

    Creating a model ARTICLE

--- 3.UPG_News_Blog_DK/models.py
+++ 3.UPG_News_Blog_DK/models.py
@@ -11,3 +11,12 @@ class User(db.Model):
 	email = db.Column(db.String(100), nullable=False)
 	hashed_password = db.Column(db.String(200), nullable=False)
 	created_date = db.Column(db.DateTime, default=datetime.utcnow)
+
+class Article(db.Model):
+	__tablename__ = 'article'
+
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(200), nullable=False)
+	text = db.Column(db.Text, nullable=False)
+	created_date = db.Column(db.DateTime, default=datetime.utcnow)
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
