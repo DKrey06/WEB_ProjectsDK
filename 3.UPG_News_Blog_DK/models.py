f86d708d508e1ac224c2afeb1d537aab245d1172
DKrey06 <mrdmitry2006@mail.ru>
2025-10-14 20:24:39 +1000

    Setting up one-to-many relationships. Adding password hashing and verification.

--- 3.UPG_News_Blog_DK/models.py
+++ 3.UPG_News_Blog_DK/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
@@ -11,6 +12,14 @@ class User(db.Model):
 	email = db.Column(db.String(100), nullable=False)
 	hashed_password = db.Column(db.String(200), nullable=False)
 	created_date = db.Column(db.DateTime, default=datetime.utcnow)
+
+	articles = db.relationship('Article', backref='author', lazy=True)
+
+	def set_password(self, password):
+		self.hashed_password = generate_password_hash(password)
+    
+	def check_password(self, password):
+		return check_password_hash(self.hashed_password, password)
 
 class Article(db.Model):
 	__tablename__ = 'article'
