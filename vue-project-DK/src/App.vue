37cacd9389cc73858dc2f4210c36e06bb2a9c887
DKrey06 <mrdmitry2006@mail.ru>
2025-11-09 13:25:50 +1000

    Creating a VUE project folder

---
+++ vue-project-DK/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <h1>Hello World</h1>
+  <div id="app">
+    <RouterView />
+  </div>
+  
+</template>
+
+<script>
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  line-height: 1.6;
+  color: #333;
+  background-color: #f8f9fa;
+}
+
+#app {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+</style>
