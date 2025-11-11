88a6b2c05fe61ad4ccd63555c16f88d28bc10046
DKrey06 <mrdmitry2006@mail.ru>
2025-11-11 22:18:22 +1000

    Connecting to the backend server

--- vue-project-DK/vite.config.js
+++ vue-project-DK/vite.config.js
@@ -1,18 +1,20 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+      '@': '/src'
+    }
+  }
 })
