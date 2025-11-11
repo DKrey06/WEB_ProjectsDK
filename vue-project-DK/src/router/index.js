c734263972ab939d28309a68cf5a6023517179cb
DKrey06 <mrdmitry2006@mail.ru>
2025-11-11 22:24:42 +1000

    add Vue Router configuration with auth guards

--- vue-project-DK/src/router/index.js
+++ vue-project-DK/src/router/index.js
@@ -2,7 +2,54 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('@/views/Home.vue')
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('@/views/About.vue')
+    },
+    {
+      path: '/articles',
+      name: 'articles',
+      component: () => import('@/views/Articles.vue')
+    },
+    {
+      path: '/articles/:id',
+      name: 'article-detail',
+      component: () => import('@/views/ArticleDetail.vue')
+    },
+    {
+      path: '/create-article',
+      name: 'create-article',
+      component: () => import('@/views/CreateArticle.vue'),
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/Login.vue')
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('@/views/Register.vue')
+    }
+  ]
+})
+
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore()
+
+  if (to.meta.requiresAuth && !authStore.isAuthenticated) {
+    next('/login')
+  } else {
+    next()
+  }
 })
 
 export default router
